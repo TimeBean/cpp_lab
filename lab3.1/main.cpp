dd21fa9012b03df4bdee2e151580d827aa28d701
Alexander <alexanderklush8@gmail.com>
2026-03-24 22:47:27 +0300

    fix(lab3.1): naming

--- lab3.1/main.cpp
+++ lab3.1/main.cpp
@@ -8,15 +8,19 @@ constexpr double STEP = 0.3;
 
 int main() {
     double x = A;
-    double y;
 
-    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Программа для вычисления значений функций в диапазоне от " << A << " до " << B << " с шагом " << STEP
+            << std::endl << std::endl;
 
     std::cout << "    +-------+-------+" << std::endl;
     std::cout << "    |   x   |   y   |" << std::endl;
     std::cout << "    +-------+-------+" << std::endl;
 
+    std::cout << std::fixed << std::setprecision(2);
+
     while (x < B) {
+        double y;
+
         if (x > 1) {
             y = exp(x);
         } else if (x < 0) {
@@ -25,10 +29,13 @@ int main() {
             y = -1;
         }
 
-        std::cout << "    | " << std::setw(5) << x << " | " << std::setw(5) << y << " |" << std::endl;
+        if (y > 0) {
+            std::cout << "    | " << std::setw(5) << x << " | " << std::setw(5) << y << " |" << std::endl;
+
+            std::cout << "    +-------+-------+" << std::endl;
+        }
 
         x += STEP;
-        std::cout << "    +-------+-------+" << std::endl;
     }
 
     return 0;
