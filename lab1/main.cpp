b1c4ba767ca775b52852776db2e37bb9ae6b7fa9
Alexander <alexanderklush8@gmail.com>
2026-03-25 17:29:20 +0300

    feat(lab1): add function to get d

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,19 +2,26 @@
 #include <cmath>
 #include <iomanip>
 
+double get_d(double x, double y) {
+    return (x * exp(x * y) + 8 * pow(sin(x), 2)) /
+           (x * (x - y) * (3 * x + y));
+}
+
 int main() {
-    std::cout << "Программа для вычисления функции. " << std::endl;
-    std::cout << "Тестовые значения: x = 1,002; y = -0.5321. Ожидается d = 1,6480." << std::endl;
+    //Тестовые значения: x = 1,002; y = -0.5321. Ожидается d = 1,6480.
+    std::cout << "Программа для вычисления формулы. " << std::endl;
 
     double x, y;
     std::cout << "Введите x > ";
-    std::cin >> x;
+    if (!(std::cin >> x)) {
+        return 1;
+    }
     std::cout << "Введите y > ";
-    std::cin >> y;
+    if (!(std::cin >> y)) {
+        return 1;
+    }
 
-    const double d = (x * exp(x * y) + 8 * pow(sin(x), 2)) /
-                     (x * (x - y) * (3 * x + y));
-
+    const double d = get_d(x, y);
     std::cout << "d = " << std::fixed << std::setprecision(4) << d << std::endl;
 
     return 0;
