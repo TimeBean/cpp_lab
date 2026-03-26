94572c1cedadb051af66a10d55c85aaf4af0f642
Alexander <alexanderklush8@gmail.com>
2026-03-26 21:37:37 +0300

    fix(lab1): styling

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -3,26 +3,40 @@
 #include <iomanip>
 
 double get_d(double x, double y) {
-    return (x * exp(x * y) + 8 * pow(sin(x), 2)) /
-           (x * (x - y) * (3 * x + y));
+    const double denominator = x * (x - y) * (3 * x + y);
+    if (std::abs(denominator) < 1e-9) {
+        throw std::runtime_error("Деление на ноль.");
+    }
+
+    const double d = (x * exp(x * y) + 8 * sin(x) * sin(x)) /
+                     denominator;
+
+    return d;
 }
 
 int main() {
     //Тестовые значения: x = 1,002; y = -0.5321. Ожидается d = 1,6480.
-    std::cout << "Программа для вычисления формулы. " << std::endl;
+    std::cout << "Программа для вычисления формулы.\n";
 
     double x, y;
-    std::cout << "Введите x > ";
+    std::cout << "Введите x: ";
     if (!(std::cin >> x)) {
+        std::cerr << "Введенная строка некорректна. Ожидалось число.\n";
         return 1;
     }
-    std::cout << "Введите y > ";
+    std::cout << "Введите y: ";
     if (!(std::cin >> y)) {
+        std::cerr << "Введенная строка некорректна. Ожидалось число.\n";
         return 1;
     }
 
-    const double d = get_d(x, y);
-    std::cout << "d = " << std::fixed << std::setprecision(4) << d << std::endl;
+    try {
+        const double d = get_d(x, y);
+        std::cout << "d = " << std::fixed << std::setprecision(4) << d << '\n';
+    } catch (std::runtime_error const &e) {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
 
     return 0;
 }
