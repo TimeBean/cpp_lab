63a8e9fe54af05bd228e38ea56b6e2294a1c7eee
Alexander <alexanderklush8@gmail.com>
2026-04-05 18:27:42 +0300

    feat(lab4): solve 4

---
+++ lab4/main.cpp
@@ -0,0 +1,47 @@
+#include <array>
+#include <iomanip>
+#include <iostream>
+#include <random>
+
+constexpr int ELEMENTS_COUNT_K = 70;
+constexpr int ELEMENT_MINIMUM_Z1 = -40;
+constexpr int ELEMENT_MAXIMUM_Z2 = 60;
+constexpr int ROWS_N = 10;
+constexpr int COLUMNS_M = 7;
+
+int main() {
+    std::mt19937 rng(std::random_device{}());
+    std::uniform_int_distribution dist(ELEMENT_MINIMUM_Z1, ELEMENT_MAXIMUM_Z2);
+
+    std::cout << "Дан массив (представлен в виде матрицы):" << std::endl;
+
+    std::array<int, ELEMENTS_COUNT_K> b{};
+    int counter = 0;
+    for (int& element : b) {
+        element = dist(rng);
+        std::cout << std::setw(3) << element << ' ';
+
+        counter++;
+        if (counter == ROWS_N) {
+            std::cout << '\n';
+            counter = 0;
+        }
+    }
+
+    int count = 0;
+    int sum = 0;
+    for (const int& element: b) {
+        if (!(element < -10 || element > 30)) {
+            continue;
+        }
+
+        count += 1;
+        sum += element;
+    }
+
+    std::cout << "\nДанные для элементов меньше -10 или больше 30:\n";
+    std::cout << "Количество: " << count << '\n';
+    std::cout << "Сумма: " << sum << '\n';
+
+    return 0;
+}
